259246139e1b3ab7a22394ae75f7f82f61126f0a
LQuesadaM <luisquesada163@gmail.com>
2024-02-16 08:16:04 -0300

    linking html elements to variables

---
+++ script.js
@@ -0,0 +1,10 @@
+const playlistSongs = document.getElementById("playlist-songs");
+const playButton = document.getElementById("play");
+const pauseButton = document.getElementById("pause");
+const nextButton = document.getElementById("next");
+const previousButton = document.getElementById("previous");
+const shuffleButton = document.getElementById("shuffle");
+
+
+
+
