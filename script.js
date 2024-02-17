61de360f78fd3cf26eb4b7d27aaf26f5f38b0d45
LQuesadaM <luisquesada163@gmail.com>
2024-02-17 08:45:27 -0300

    Before playing the song, sure it starts from the beginning.

--- script.js
+++ script.js
@@ -92,7 +92,11 @@ const playSong = (id) => {
     audio.src = song.src;
     audio.title = song.title;
 
-    
+    if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
+        audio.currentTime = 0;
+    } else {
+        audio.currentTime = userData?.songCurrentTime;
+    }
 };
 
 const renderSongs = (array) => {
