5aeac7c213168216c6736738bd299bda86e1ffa4
LQuesadaM <luisquesada163@gmail.com>
2024-02-17 21:21:54 -0300

    change song

--- script.js
+++ script.js
@@ -114,6 +114,10 @@ const pauseSong = () => {
 const playNextSong = () => {
     if (userData?.currentSong === null) {
         playSong(userData?.songs[0].id);
+    } else {
+        const currentSongIndex = getCurrentSongIndex();
+        const nextSong = userData?.songs[currentSongIndex + 1];
+        playSong(nextSong.id);
     }
 };
 
